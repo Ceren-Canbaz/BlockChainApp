7540d01acd9032d817def36bbfd66b5ebc04ee83
candeithdroid <ceren_canbaz@hotmail.com>
2021-12-17 02:03:04 +0300

    Proje dosyası ekle.

---
+++ BlockChainAppMvc/Models/Coins.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+	public class Coins
+	{
+		public int Id { get; set; }
+		public string coinName { get; set; }
+		public double coinValue { get; set; }
+	}
+}
---
+++ BlockChainAppMvc/Models/Wallet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockChainAppMvc.Models
+{
+	public class Wallet
+	{
+		public int Id { get; set; }
+		public double balance { get; set; }
+	}
+}
